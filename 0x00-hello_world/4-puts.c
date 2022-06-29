789b3d62a5363a65f60cf8d61a59fa2c6d8c2135
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-06-29 03:18:04 -0700

    task 4 commit 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * this is just a description for my code 
+ * according to the exercice i tried to write a program that can print this messa
+ * messgage
+ * Programming is like building a multilingual puzzle
+ * by using this function puts
+ *
+ */
 int main(void)
 {
 
