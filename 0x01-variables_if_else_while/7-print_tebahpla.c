c8a1be9af52fbceb83e7ea8d4d5307eb100e798f
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-03 15:13:05 -0700

    task 7 v1

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,29 +1,15 @@
 #include<stdio.h>
-
 /**
- *
- * * main - Prints the alphabet.
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+* main - Prints the alphabet.
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
 	int i;
-
 	for (i = 122; i > 96; i--)
-
 	{
-
 		putchar(i);
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
