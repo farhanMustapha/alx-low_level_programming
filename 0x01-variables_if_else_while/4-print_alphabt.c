2a4e2b98aec30e41c8acbc6ca02c8dc722c3d11b
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-03 05:08:07 -0700

    task 4 v1

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints the alphabet without q and e.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 123; i++)
+{
+if (i != 4 && i !=16 )
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
