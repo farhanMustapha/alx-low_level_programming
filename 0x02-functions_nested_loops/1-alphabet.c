9b7f7ad6ad0270fc88f0732d0f227c4d54d68ec7
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-09 13:52:54 -0700

    task 1 i'm late sorry

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times
- * Return: Always 0 (Success)
+ * print_alphabet -> Prints the alphabets
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-int a, b;
-for (a = 0; a <= 9; a++)
+int j;
+for (j = 'a'; j <= 'z'; j++)
 {
-for (b = 97; b <= 122; b++)
-{
-_putchar(b);
+_putchar(j);
 }
 _putchar('\n');
-}
 }
