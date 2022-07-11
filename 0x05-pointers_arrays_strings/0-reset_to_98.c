a5411b0deb25f97be95cc6fd9d8fef877465971d
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-11 08:08:24 -0700

    task 1 ajout redme file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 function that update the value
- * of the ponter
- * @n: is a parameter
+ * reset_to_98 update the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ * Return: nothing
  */
 void reset_to_98(int *n)
 {
