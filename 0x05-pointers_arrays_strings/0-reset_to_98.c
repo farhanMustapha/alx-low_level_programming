377d07efa900e838cda6add36c1cec48f56b83b7
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-11 05:33:04 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include main.h
+
+/**
+ * reset_to_98 function that update the value
+ * of the ponter
+ * @n: is a parameter
+ */
+void reset_to_98(int *n)
+{
+int p;
+p = 98;
+n = &p;
+return (0);
+}
