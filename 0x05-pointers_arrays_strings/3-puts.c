9b6b6ec83aff6bb069198127c138d3e42c36e376
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-11 15:34:56 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - function that print string 
+ * @str: first parameter
+ * return: no thing
+ */
+void _puts(char *str)
+{
+printf("the string is : %d\n ", *str);
+}
