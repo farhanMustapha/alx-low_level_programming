92f82de9456c40094f10c2fe1dc72d3e907422de
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-12 13:19:59 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - function that copies the string pointed
+ *  @dest: first parameter
+ *  @src: second parameter
+ *  return (dest)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i]; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = src[i];
+return (dest);
+}
+
