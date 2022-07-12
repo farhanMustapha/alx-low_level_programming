dcd903be1a624324574be2485ab411b94a66e522
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-12 13:25:32 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@
  * _strcpy - function that copies the string pointed
  *  @dest: first parameter
  *  @src: second parameter
- *  return: Always 0
+ *  Return: Always 0.
  */
 
 char *_strcpy(char *dest, char *src)
