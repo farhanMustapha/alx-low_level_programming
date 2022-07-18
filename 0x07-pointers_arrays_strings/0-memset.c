7178d93efaaae5b0210957fd83ed166419dc3fd8
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-18 13:31:03 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * memeset is a  function that fills memory with a constant byte.
+ * @s: first entry
+ * @b: second entry
+ * @n: third entry
+ * Return s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+{
+s[a] = b;
+}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+hello from 07  file week from 18/07 to 22/07
