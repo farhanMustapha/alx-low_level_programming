da9c1869ef11d252504517463062a85d0e7fade9
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-07-26 13:20:33 -0700

    task 0 v1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,7 @@
  * create_array - creates an array of chars
  * @size: The size of the array
  * @c: The char to fill in the array
+ *
  * Return: The array filled
  */
 char *create_array(unsigned int size, char c)
